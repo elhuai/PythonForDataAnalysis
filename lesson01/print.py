9bb8af3cc3ddc36e3b9f82b731ba69e67ae01736
huai <huai@huaideMacBook-Air.local>
2025-05-03 13:58:19 +0800

    新增lesson1_print使用

---
+++ lesson01/print.py
@@ -0,0 +1,15 @@
+#print
+print("Hello, World!")
+print("Hello, Python!")
+
+
+# 數字運算
+print(3+7)
+print(10-2)
+print(4*5)
+print(20/4)
+print(2**3) #次方
+print(10 % 3) #餘數
+print(10 // 3) #整數除法
+
+# 邏輯運算
